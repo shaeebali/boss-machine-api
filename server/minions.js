c35c1c1923fbe44dbdcd6d7a61d605a30b1ced1c
shaeeb <shaeebali@gmail.com>
2023-11-27 10:53:37 +0000

    add router.param for minions to check id

--- server/minions.js
+++ server/minions.js
@@ -2,6 +2,16 @@ const express = require('express');
 const { getAllFromDatabase, getFromDatabaseById, addToDatabase, updateInstanceInDatabase, deleteFromDatabasebyId } = require('./db');
 const minionsRouter = express.Router();
 
+minionsRouter.param('minionId',  (req, res, next, id) => {
+  const minion = getFromDatabaseById('minions', id);
+  if (minion) {
+    req.minion = minion;
+    next();
+  } else {
+    res.status(404).send();
+  }
+})
+
 minionsRouter.get('/', (req, res, next) => {
   res.send(getAllFromDatabase('minions'));
   console.log(getAllFromDatabase('minions'))
@@ -12,6 +22,8 @@ minionsRouter.post('/', (req, res, next) => {
   res.status(201).send(minion);
 })
 
+minionsRouter.get('/:id', (req, res, next) => {
 
+})
 
 module.exports = minionsRouter;
