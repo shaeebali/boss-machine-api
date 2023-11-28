e5ec3624d0a1a31c00ca4efc3f791e5737cd30f5
shaeeb <shaeebali@gmail.com>
2023-11-28 09:26:49 +0000

    setup routes/exports for ideas.js

--- server/api.js
+++ server/api.js
@@ -4,8 +4,10 @@ const { getAllFromDatabase } = require('./db');
 
 // Import routers below from files
 const minionsRouter = require('./minions');
+const ideasRouter = require('./ideas');
 
 // Declare use of each router below
 apiRouter.use('/minions', minionsRouter);
+apiRouter.use('/ideas', ideasRouter);
 
 module.exports = apiRouter;
