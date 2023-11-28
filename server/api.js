602dc9fdf35d78aed7ccd7cb053b1b52fe41e32a
shaeeb <shaeebali@gmail.com>
2023-11-28 10:06:52 +0000

    complete meetings.js CRUD operations/routes

--- server/api.js
+++ server/api.js
@@ -5,9 +5,11 @@ const { getAllFromDatabase } = require('./db');
 // Import routers below from files
 const minionsRouter = require('./minions');
 const ideasRouter = require('./ideas');
+const mettingsRouter = require('./meetings');
 
 // Declare use of each router below
 apiRouter.use('/minions', minionsRouter);
 apiRouter.use('/ideas', ideasRouter);
+apiRouter.use('/meetings', mettingsRouter);
 
 module.exports = apiRouter;
